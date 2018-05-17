9cb0cbd07484b159c2acc1d5de3362f8d1bb5164
Priyanshu Nayan <priyanshunayan456@gmail.com>
2018-05-17 13:51:21 +0530

    url change

--- app.js
+++ app.js
@@ -5,7 +5,7 @@
 
 
 // JSON Extraction using XMLHttpRequest() Technique. Move to fetch API sometime later.
-var requestURL = "https://api.myjson.com/bins/f7tj2";
+var requestURL = "https://api.myjson.com/bins/btndq";
 var request = new XMLHttpRequest();
 request.open('GET',requestURL, true);
 request.responseType = '';
@@ -14,9 +14,8 @@ request.onload = function() {
 if (request.status>=200 && request.status<400) {
 	var data = JSON.parse(request.responseText);
 	console.log(data);
-	console.log(data.address.city);
-	console.log(data.firstName);
-	console.log(data.phoneNumbers[0].number+ " " + data.phoneNumbers[0].type)
+	console.log(data[0].text);
+
 } else {
 	console.log("We were halfway down still there was error.")
 }
