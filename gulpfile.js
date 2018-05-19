eff314bc738f24febfac171a20ae44248c007d1d
Priyanshu Nayan <priyanshunayan456@gmail.com>
2018-05-19 10:04:07 +0530

    gulp workflow add

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+var gulp = require('gulp');
+
+var browserSync = require('browser-sync').create();
+gulp.task('browserSync', function() {
+  browserSync.init({
+    server: {
+      baseDir: 'app'
+    },
+  })
+})
+
+gulp.task('watch', ['browserSync'], function (){
+  //gulp.watch('app/scss/**/*.scss', ['sass']); 
+  // Reloads the browser whenever HTML or JS files change
+  gulp.watch('app/*.html', browserSync.reload); 
+  gulp.watch('app/js/**/*.js', browserSync.reload); 
+});
