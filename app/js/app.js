bc27f5bbbc149349796a1448a2f2537837d4672b
Priyanshu Nayan <priyanshunayan456@gmail.com>
2018-08-17 23:42:35 +0530

    data fetch for words and their meanings

--- app/js/app.js
+++ app/js/app.js
@@ -1,81 +1,48 @@
-// Firbase integration for signup and login functionality.
-// library use for graphs and user specific stats.
-var questions = [];
-var i=0; var wrongDef;
-var score = [];
-var correctAns = [];
-var wrongAns = [];
-console.log("Hello world");
-
-
-//integration of wordnik api
-
-const key = "f56da904961d843d0300007813f06a10f056eadd8f42c78c8";
-var words, wordList;
-
-//thiscomment
-// JSON Extraction using XMLHttpRequest() Technique. Move to fetch API sometime later.
-
-//Word Extraction
-const wordURL = "https://api.wordnik.com/v4/words.json/randomWords?hasDictionaryDef=true&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=5&maxLength=-1&limit=10&api_key="+key;
-const wordRequest = new XMLHttpRequest();
-wordRequest.open('GET', wordURL, true);
-
-wordRequest.send();
-wordRequest.onload = function() {
-	if (wordRequest.status>=200 && wordRequest.status<400) {
-    wordList = JSON.parse(this.response);
-	console.log(wordList);
-	wordList.forEach(function(arrayItem){
-		questions.push(arrayItem.word);
+(function main() {
+	let wordsArray = [];
+	let meaningsArray = [];
+	
+	const key = "f56da904961d843d0300007813f06a10f056eadd8f42c78c8";
+	const wordURL = "https://api.wordnik.com/v4/words.json/randomWords?hasDictionaryDef=true&excludePartOfSpeech=proper-noun%2C%20proper-noun-plural%2C%20proper-noun-posessive%2C%20family-name&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=5&maxLength=-1&limit=10&api_key="
+	+key;
+	
+	fetch(wordURL)
+	.then((data)=> {
+		return data.json();
 	})
-}
-	// store correct Definitions in correctAns array.
-		for(i=0;i<5;i++){
-		wordsNew = questions[i];
-		let urlNew = "https://api.wordnik.com/v4/word.json/"+wordsNew+"/definitions?limit=200&includeRelated=false&useCanonical=false&includeTags=false&api_key="+key;
-		let requestURLNew = urlNew;
-		let requestNew = new XMLHttpRequest();
-		requestNew.open('GET',requestURLNew, true);
-		requestNew.send();
-		requestNew.onload = function() {
-		if (requestNew.status>=200 && requestNew.status<400) {
-		var dataNew = JSON.parse(this.response);
-		//console.log(dataNew);
-		correctDefNew = dataNew[0].text;
-		correctAns.push(correctDefNew);
+	.then(words => {
+		words.forEach(word => {
+			wordsArray.push(word.word);
+		});
+		console.log(wordsArray);
+	}).then(()=>{
+		if(wordsArray.length === 10) {
+		retrieveMeaning();
+		}
+	}).then(() => {
+		document.getElementById('question').innerHTML = "What is the meaning of " + wordsArray[0] + " ?";
+	})
+	.catch(err => {
+		console.log(err);
+	})
 
+
+	const retrieveMeaning = () => {
+		if(wordsArray.length === 10) {
+		wordsArray.forEach(word => {
+			const meaningURL = "https://api.wordnik.com/v4/word.json/"
+			+ word + "/definitions?limit=200&includeRelated=true&sourceDictionaries=all&useCanonical=false&includeTags=false&api_key=" + key
+			fetch(meaningURL).then(data => {
+				return data.json();
+			}).then(meaning=> {
+				meaningsArray.push(meaning[0].text);
+			})
+			.catch(err => {
+				console.log(err);
+			})
+		})
 		}
 	}
-}
-		//Store Wrong Answers in wrongAns Array
-		for(i=5;i<10;i++) {
-		let wordsWrongNew = questions[i];
-		let wordsWrongurlNew = "https://api.wordnik.com/v4/word.json/"+wordsWrongNew+"/definitions?limit=200&includeRelated=false&useCanonical=false&includeTags=false&api_key="+key;
-		let wordsWrongrequestURLNew = wordsWrongurlNew;
-		let wordsWrongrequestNew = new XMLHttpRequest();
-		wordsWrongrequestNew.open('GET',wordsWrongrequestURLNew, true);
-		wordsWrongrequestNew.send();
-		wordsWrongrequestNew.onload = function() {
-		if (wordsWrongrequestNew.status>=200 && wordsWrongrequestNew.status<400) {
-		var wordsWrongdataNew = JSON.parse(this.response);
-		wrongDefNew = wordsWrongdataNew[0].text;
-		wrongAns.push(wrongDefNew);
-		}
-	}
-}
-};
-
-console.log(wrongAns);
-console.log(questions);
-console.log(correctAns);
-
-//Whooa Now we have three arrays one having the question, another having the correct option and another having the wrong options......
-//Begin the quiz thing here. You have all the necessary Data with you bro.
-
-
-
-
-
-
-
+	
+	
+}());
