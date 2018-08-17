1366e46d1f7e49f4e48c0b1d432500367ac9442f
Priyanshu Nayan <priyanshunayan456@gmail.com>
2018-08-18 00:44:56 +0530

    problem in asynchronous js

--- app/js/app.js
+++ app/js/app.js
@@ -1,4 +1,4 @@
-(function main() {
+/* (function main() {
 	let wordsArray = [];
 	let meaningsArray = [];
 	
@@ -45,4 +45,56 @@
 	}
 	
 	
-}());
+}()); */
+
+const getWords = (wordsArray) => {
+	const key = "f56da904961d843d0300007813f06a10f056eadd8f42c78c8";
+	const wordURL = "https://api.wordnik.com/v4/words.json/randomWords?hasDictionaryDef=true&excludePartOfSpeech=proper-noun%2C%20proper-noun-plural%2C%20proper-noun-posessive%2C%20family-name&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=5&maxLength=-1&limit=10&api_key="
+	+key;
+	
+	fetch(wordURL)
+	.then((data)=> {
+		return data.json();
+	})
+	.then(words => {
+		words.forEach(word => {
+			wordsArray.push(word.word);
+		});
+		return wordsArray;
+	})
+	.catch(err => {
+		console.log(err);
+	})
+
+}
+
+const getMeaning = (wordsArray, meaningsArray) => {
+	console.log("I got called");
+	const key = "f56da904961d843d0300007813f06a10f056eadd8f42c78c8";
+		wordsArray.forEach(word => {
+			const meaningURL = "https://api.wordnik.com/v4/word.json/"
+			+ word + "/definitions?limit=200&includeRelated=true&sourceDictionaries=all&useCanonical=false&includeTags=false&api_key=" + key
+			fetch(meaningURL).then(data => {
+				return data.json();
+			}).then(meaning=> {
+				meaningsArray.push(meaning[0].text);
+			})
+			.catch(err => {
+				console.log(err);
+			})
+		})
+
+}
+
+function main() {
+	let wordsArray = [];
+	let meaningsArray = [];
+
+	getWords(wordsArray);
+	console.log(getWords(wordsArray) instanceof Promise);
+	getMeaning(wordsArray, meaningsArray);
+
+	console.log(meaningsArray);
+}
+
+main();
