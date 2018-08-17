1ff4ed39ac3f02ea4a82a0dc5e7f853c404084b4
Priyanshu Nayan <priyanshunayan456@gmail.com>
2018-08-18 01:04:40 +0530

    no luck till now

--- app/js/app.js
+++ app/js/app.js
@@ -1,7 +1,7 @@
 (function main() {
 	let wordsArray = [];
 	let meaningsArray = [];
-	
+
 	const key = "f56da904961d843d0300007813f06a10f056eadd8f42c78c8";
 	const wordURL = "https://api.wordnik.com/v4/words.json/randomWords?hasDictionaryDef=true&excludePartOfSpeech=proper-noun%2C%20proper-noun-plural%2C%20proper-noun-posessive%2C%20family-name&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=5&maxLength=-1&limit=10&api_key=" +
 		key;
@@ -16,36 +16,50 @@
 			});
 			console.log(wordsArray);
 		}).then(() => {
-			retrieveMeaning(wordsArray, meaningsArray);
-		}).then( () => {
-			
+			const key = "f56da904961d843d0300007813f06a10f056eadd8f42c78c8";
+			if (wordsArray.length === 10) {
+				let count = 0;
+				wordsArray.forEach(word => {
+					const meaningURL = "https://api.wordnik.com/v4/word.json/" +
+						word + "/definitions?limit=200&includeRelated=true&sourceDictionaries=all&useCanonical=false&includeTags=false&api_key=" + key
+					fetch(meaningURL).then(data => {
+							console.log(count++);
+							const meaning = data.json();
+							console.log("===============>");
+							meaningsArray.push(meaning);
+						})
+				})
+			}
+		})
+		.then(() => {
 			console.log(meaningsArray);
 		})
 		.catch(err => {
 			console.log(err);
 		})
-		const displayQuestion = (arrayOfWords, arrayOfMeanings) => {
-			if(arrayOfMeanings!=10 || arrayOfWords!= 10) {
-				console.log("Hi");
-				const loader = document.getElementsByClassName('center-loader');
-				document.getElementById('question-container').style.display = "none";
-			}
-			if(arrayOfMeanings === 10 && arrayOfWords === 10) {
-				const loader = document.getElementsByClassName('center-loader');
-				loader[0].style.display = "none";
-				document.getElementById('question-container').style.display = "block";
-			}
-		
+	const displayQuestion = (arrayOfWords, arrayOfMeanings) => {
+		if (arrayOfMeanings != 10 || arrayOfWords != 10) {
+			console.log("Hi");
+			const loader = document.getElementsByClassName('center-loader');
+			document.getElementById('question-container').style.display = "none";
 		}
-		displayQuestion(wordsArray, meaningsArray);
-		
+		if (arrayOfMeanings === 10 && arrayOfWords === 10) {
+			const loader = document.getElementsByClassName('center-loader');
+			loader[0].style.display = "none";
+			document.getElementById('question-container').style.display = "block";
+		}
+
+	}
+	//displayQuestion(wordsArray, meaningsArray);
+
 
 }());
 
-const retrieveMeaning = (arrayOfWords, arrayOfMeanings) => {
+
+/* const retrieveMeaning = (arrayOfWords, arrayOfMeanings) => {
 	const key = "f56da904961d843d0300007813f06a10f056eadd8f42c78c8";
 	if (arrayOfWords.length === 10) {
-		let count =0;
+		let count = 0;
 		arrayOfWords.forEach(word => {
 			const meaningURL = "https://api.wordnik.com/v4/word.json/" +
 				word + "/definitions?limit=200&includeRelated=true&sourceDictionaries=all&useCanonical=false&includeTags=false&api_key=" + key
@@ -61,14 +75,14 @@ const retrieveMeaning = (arrayOfWords, arrayOfMeanings) => {
 		})
 	}
 }
-
+}) */
 const displayQuestion = (arrayOfWords, arrayOfMeanings) => {
-	if(arrayOfMeanings!=10 || arrayOfWords!= 10) {
+	if (arrayOfMeanings != 10 || arrayOfWords != 10) {
 		console.log("Hi");
 		const loader = document.getElementsByClassName('center-loader');
 		document.getElementById('question-container').style.display = "none";
 	}
-	if(arrayOfMeanings === 10 && arrayOfWords === 10) {
+	if (arrayOfMeanings === 10 && arrayOfWords === 10) {
 		const loader = document.getElementsByClassName('center-loader');
 		loader[0].style.display = "none";
 		document.getElementById('question-container').style.display = "block";
