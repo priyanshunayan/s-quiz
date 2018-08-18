5f721eedabdee13ee8d1553b3f3f38a681ec0976
Priyanshu Nayan <priyanshunayan456@gmail.com>
2018-08-18 15:26:50 +0530

    Quiz questions displaying and calculating scores

--- app/js/app.js
+++ app/js/app.js
@@ -1,4 +1,6 @@
 //to get the words
+const wordMeaning = {};
+const scores = [];
 let haveMeaningsLoaded = false;
 const getWords = (wordsArray) => {
 	const promise = new Promise((resolve, reject) => {
@@ -36,19 +38,21 @@ const getMeaning = (wordsArray, meaningsArray) => {
 					})
 				.then(meaning => {
 					count++;
-					console.log("message");
+					console.log("message", wordsArray[i]);
+					wordMeaning[wordsArray[i]] = meaning[0].text;
 					meaningsArray.push(meaning[0].text);
-					if(count === 10) {
+					if (count === 10) {
 						haveMeaningsLoaded = true;
 						console.log("have");
 						hideLoader();
+						showQuestions(wordsArray, meaningsArray);
 					}
 				})
 				.catch(err => {
 					console.log(err);
 				})
 			console.log(i);
-		} 
+		}
 		resolve(meaningsArray);
 	})
 	console.log("From thre Promise", meaningsArray);
@@ -58,18 +62,114 @@ const getMeaning = (wordsArray, meaningsArray) => {
 
 function showLoader() {
 	document.addEventListener("DOMContentLoaded", () => {
+		const x = document.getElementById('x');
+		const questionStatus = document.getElementById("question-status");
 		x.style.display = "none";
+		questionStatus.style.display = "none";
 	})
 }
 
 function hideLoader() {
 	console.log("O am called");
-		const loader = document.getElementsByClassName('center-loader');
-		loader[0].style.display = "none";
+	const loader = document.getElementsByClassName('center-loader');
+	const questionStatus = document.getElementById("question-status");
+	loader[0].style.display = "none";
+	const x = document.getElementById('x');
+	x.style.display = "block";
+	questionStatus.style.display = "table-cell";
+	console.log("yaya");
+
+}
+let count = 0;
+const showQuestions = (wordsArray, meaningsArray) => {
+	console.log("Object ======>", wordMeaning);
+	const questionWord = Object.keys(wordMeaning).slice(0, 5);
+	const answerArray = Object.values(wordMeaning).slice(0, 5);
+	const wrongAnswerArray = Object.values(wordMeaning).slice(5, 10);
+	const questionField = document.getElementById('question');
+	const option1 = document.getElementById("option1");
+	const option2 = document.getElementById("option2");
+	let score = 0;
+	questionField.innerHTML = "What is the meaning of " + questionWord[0] + " ?";
+	//Randomly assign the options.
+	if (Math.random() > 0.5) {
+		option1.innerHTML = answerArray[0];
+		option2.innerHTML = wrongAnswerArray[0]
+	} else {
+		option2.innerHTML = answerArray[0];
+		option1.innerHTML = wrongAnswerArray[0]
+	}
+
+
+	option1.addEventListener('click', () => {
+		count++;
+
+		if (count < 6) {
+			calcScores(option1, answerArray);
+			questionField.innerHTML = "What is the meaning of " + questionWord[count] + " ?";
+			let num = Math.random();
+			if (num > 0.5) {
+				option1.innerHTML = answerArray[count];
+				option2.innerHTML = wrongAnswerArray[count]
+			}
+			if (num < 0.5) {
+				option2.innerHTML = answerArray[count];
+				option1.innerHTML = wrongAnswerArray[count]
+			}
+			document.getElementById('count').innerHTML = count + 1;
+		}
+		displayResults();
+	})
+	option2.addEventListener('click', () => {
+		count++;
+
+		if (count < 6) {
+			calcScores(option2, answerArray);
+			questionField.innerHTML = "What is the meaning of " + questionWord[count] + " ?";
+			let num = Math.random();
+			if (num > 0.5) {
+				option1.innerHTML = answerArray[count];
+				option2.innerHTML = wrongAnswerArray[count]
+			}
+			if (num < 0.5) {
+				option2.innerHTML = answerArray[count];
+				option1.innerHTML = wrongAnswerArray[count]
+			}
+			document.getElementById('count').innerHTML = count + 1;
+		}
+		displayResults();
+	})
+}
+let score = 0;
+
+const calcScores = (option, answers) => {
+	if (answers.includes(option.textContent)) {
+		score++;
+		console.log("score ===>", score);
+	}
+	console.log("Hi");
+}
+
+const displayResults = () => {
+	console.log(count);
+	if (count === 5) {
 		const x = document.getElementById('x');
-		x.style.display = "block";
-		console.log("yaya");
-	
+		const questionStatus = document.getElementById("question-status");
+		x.style.display = "none";
+		questionStatus.style.display = "none";
+		document.getElementById('score').innerHTML = "Your score: " + score * 50 + "/250";
+		document.getElementById('revisit').innerHTML = "Revisit Words";
+		let loopCount = 0;
+		for (var key in wordMeaning) {
+			if (wordMeaning.hasOwnProperty(key)) {
+				loopCount++;
+				if (loopCount < 6) {
+					document.getElementById('words').innerHTML += "<b>" + key + "</b>" + " :" + wordMeaning[key] + "<br>" + "<br>";
+				}
+			}
+		}
+
+	}
 }
 
 async function main() {
